73b3095e5fe6cbabd8f900c494c5174d475e3ecd
Yuriy Syrovetskiy <cblp@cblp.su>
2020-07-14 21:19:38 +0300

    Add real tests

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,1 +1,14 @@
-main = pure ()
+import           Test.Tasty       (TestTree, defaultMain, testGroup)
+import           Test.Tasty.HUnit (testCase, (@?=))
+
+import           Sqeq
+
+main :: IO ()
+main =
+  defaultMain $
+    testGroup "solveSquare"
+      [ testCase "x² + 2x + 1 = 0 -> [-1, -1]" $
+          solveSquare 1 2 1 @?= [-1, -1]
+      , testCase "x² - 2x + 1 = 0 -> [1, 1]" $
+          solveSquare 1 (-2) 1 @?= [1, 1]
+      ]
