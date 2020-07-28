b9ee4af8eb86520d20eb6c82cf657ae61f00ad9a
Yuriy Syrovetskiy <cblp@cblp.su>
2020-07-28 21:21:11 +0300

    Add optparse-applicative

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,17 +1,44 @@
-import           System.Environment (getArgs, getProgName)
-import           System.Exit        (die)
+import           Control.Applicative ((<**>))
+import           Options.Applicative (Parser, argument, auto, execParser,
+                                      fullDesc, header, help, helper, info,
+                                      long, metavar, option, progDesc, short,
+                                      value)
+import           System.Environment  (getArgs, getProgName)
+import           System.Exit         (die)
 
-import           Sqeq               (solveSquare)
+import           Sqeq                (solveSquare)
+
+data Options = Options
+  { a, b, c  :: Double
+  , language :: Language
+  }
+
+data Language = En | Ru
+  deriving (Read, Show)
 
 main :: IO ()
 main = do
-  prog <- getProgName
-  args <- getArgs
-  case args of
-    [as, bs, cs] ->
-      let
-        a = read as
-        b = read bs
-        c = read cs
-      in putStrLn $ "Solution: " ++ show (solveSquare a b c)
-    _ -> die $ "Usage: " ++ prog ++ " NUMBER NUMBER NUMBER"
+  Options{a = a, b = b, c = c, language = language} <- execParser parserInfo
+  let solution =
+        case language of
+          En -> "Solution"
+          Ru -> "Ответ"
+  putStrLn $ solution ++ ": " ++ show (solveSquare a b c)
+
+parserInfo =
+  info
+    (parser <**> helper)
+    (fullDesc <> header "Solve square equation")
+
+parser :: Parser Options
+parser =
+  Options
+  <$> argument auto (metavar "A" <> help "x^2 coefficient")
+  <*> argument auto (metavar "B" <> help "x coefficient")
+  <*> argument auto (metavar "C" <> help "free term")
+  <*> option   auto (   metavar "LANGUAGE"
+                    <>  help "Language: En (default), Ru"
+                    <>  value En
+                    <>  long "language"
+                    <>  short 'l'
+                    )
